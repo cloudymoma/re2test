769e6d80a49dd8431d2f1758a3d7fa2f781f3cf1
Bin Wu <binwu@google.com>
2020-07-28 15:32:22 +0000

    updated readme and tests specifially to GCS lifetime evaluation method

--- re2test.cpp
+++ re2test.cpp
@@ -21,8 +21,41 @@ int main(int argc, char* argv[]) {
     cout << "Google re2 tests" << endl;
 
     // (text, regex)
+
+/*
     cout << RE2::FullMatch("abc", "abc") << endl;
     cout << RE2::FullMatch("abcd", "^abc") << endl;
+    cout << RE2::FullMatch("abcd", "^abcd") << endl;
+    cout << RE2::FullMatch("bcd", "bcd$") << endl;
+    cout << RE2::FullMatch("abcd", "bcd$") << endl;
+*/
+
+/*
+    cout << RE2::FullMatch("abcde", "bcd") << endl;
+    cout << RE2::FullMatch("abcde", ".bcd.") << endl;
+    cout << RE2::FullMatch("abcde", ".bcd") << endl;
+    cout << RE2::FullMatch("abcde", "^a.*d.") << endl;
+    cout << RE2::FullMatch("abcde", ".de") << endl;
+    cout << RE2::FullMatch("abcde", ".*de") << endl;
+*/
+
+    //char* pattern = "^live_g.*";
+    //char* pattern = "^live_g/.*";
+    char* pattern = "^live_g\/.*";
+    cout << RE2::FullMatch("live_g/abc.mp4", pattern) << endl;
+    cout << RE2::FullMatch("live_gabc.mp4", pattern) << endl;
+    cout << RE2::FullMatch("vod/live_g/abc.mp4", pattern) << endl;
+    cout << RE2::FullMatch("vod/cn/abc.mp4", pattern) << endl;
+    cout << RE2::FullMatch("vod/us/abc.mp4", pattern) << endl;
+
+/*
+    cout << RE2::FullMatch("abc.txt", ".*\.(jpg|png)") << endl;
+    cout << RE2::FullMatch("abc.txt", ".*\\.(jpg|png)") << endl;
+    cout << RE2::FullMatch("abc.png", ".*\.(jpg|png)") << endl;
+    cout << RE2::FullMatch("abc.png", ".*\\.(jpg|png)") << endl;
+*/
+
+/*
     cout << RE2::PartialMatch("abc", "^abc") << endl;
     cout << RE2::PartialMatch("abc", "abc$") << endl;
     cout << RE2::PartialMatch("abcd", "abc") << endl;
@@ -35,7 +68,7 @@ int main(int argc, char* argv[]) {
     cout << RE2::PartialMatch("abc.txt", ".*\\.(jpg|png)") << endl;
     cout << RE2::PartialMatch("abc.png", ".*\.(jpg|png)") << endl;
     cout << RE2::PartialMatch("abc.png", ".*\\.(jpg|png)") << endl;
-
+*/
     return 0;
 }
 
--- readme.md
+++ readme.md
@@ -8,4 +8,6 @@ make run
 
 for simple tests.
 
-Update the [code](https://github.com/cloudymoma/re2test/blob/master/re2test.cpp) accordingly for the test you want to run. Consult the APIs [here](https://github.com/google/re2/wiki/CplusplusAPI)
+Update the [code](https://github.com/cloudymoma/re2test/blob/master/re2test.cpp) accordingly for the test you want to run. Consult the [APIs](https://github.com/google/re2/wiki/CplusplusAPI)
+
+[Syntax](https://github.com/google/re2/wiki/Syntax) references
